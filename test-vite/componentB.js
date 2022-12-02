609c6879544e03e28d1c47abaad90fbe526f7e06
wffchong <283953455@qq.com>
2022-12-02 10:37:34 +0800

    feat: vite对css以及css模块化的简单处理

---
+++ test-vite/componentB.js
@@ -0,0 +1,7 @@
+import componentBCss from './componentB.module.css'
+
+const div = document.createElement('div')
+
+document.body.appendChild(div)
+
+div.className = componentBCss['footer-content']
--- test-vite/main.js
+++ test-vite/main.js
@@ -1,7 +1,10 @@
 // import { num } from './counter.js'
 // import _ from 'lodash'
 // import lodashEs from 'lodash-es'
-import './request'
+// import './request'
+// import './index.css'
+import './componentA'
+import './componentB'
 
 // console.log(num)
 // console.log(_)
